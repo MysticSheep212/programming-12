2ff798a710bb3ebd72d7b1e118a2b5a92e52fe3b
Dylan <123783386+MysticSheep212@users.noreply.github.com>
2025-05-01 23:26:46 -0700

    Uploaded Python Chapters 1-9

---
+++ Python Chapter 1/boom.py
@@ -0,0 +1,12 @@
+booms = 5
+for booms in range(1,booms+1):
+    print(booms, ' booms')
+
+
+# outer loop
+for i in range(1, 11):
+# nested loop
+    for j in range(1, 11):
+# print multiplication
+        print(i * j, end=' ')
+print()
---
+++ Python Chapter 3/test.py
@@ -0,0 +1,4 @@
+# write your code here
+for i in range(3,0, -1):
+    print(f"{i} ...")
+print("Blastoff!")
